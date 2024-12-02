62374a3607ca7033a9e574600834e9e0f6bd194c
StubblySeeker <148018304+StubblySeeker@users.noreply.github.com>
2024-12-02 18:08:21 -0500

    Fixed Joe page Link

--- app/src/Home.py
+++ app/src/Home.py
@@ -55,7 +55,7 @@ if st.button("Act as Joe Wellington, a regular Co-op seeker",
     # finally, we ask streamlit to switch to another page, in this case, the 
     # landing page for this particular user type
     logger.info("Logging in as Political Strategy Advisor Persona")
-    st.switch_page('pages/06_Joe_Home.py')
+    st.switch_page('pages/00_Pol_Strat_Home.py')
 
 if st.button('Act as Josh Brown, Data-Driven Applicant', 
             type = 'primary', 
