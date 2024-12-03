0aead95bff20e734dd29b49dfd1e24b8b413dfaa
Robel Wondwosen <robelw135@gmail.com>
2024-12-03 16:39:33 -0500

    Kyrie home

--- app/src/pages/05_Kyrie_Home.py
+++ app/src/pages/05_Kyrie_Home.py
@@ -3,10 +3,10 @@ logger = logging.getLogger(__name__)
 
 import streamlit as st
 from modules.nav import SideBarLinks
-import requests
 
 st.set_page_config(layout = 'wide')
 
+# Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
 st.title(f"Welcome, Kyrie.")
@@ -18,3 +18,8 @@ if st.button('View World Bank Data Visualization',
              type='primary',
              use_container_width=True):
   st.switch_page('pages/01_World_Bank_Viz.py')
+
+if st.button('View World Map Demo', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/02_Map_Demo.py')
