fb3c7a395bdea73b685320f1c7923752960e902c
Robel Wondwosen <robelw135@gmail.com>
2024-12-03 16:20:27 -0500

    Kyrie view within home page

--- app/src/pages/05_Kyrie_Home.py
+++ app/src/pages/05_Kyrie_Home.py
@@ -9,9 +9,12 @@ st.set_page_config(layout = 'wide')
 
 SideBarLinks()
 
-st.title('Kyrie Irving Home Page')
-
 st.title(f"Welcome, Kyrie.")
 st.write('')
 st.write('')
 st.write('### What would you like to do today?')
+
+if st.button('View World Bank Data Visualization', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/01_World_Bank_Viz.py')
