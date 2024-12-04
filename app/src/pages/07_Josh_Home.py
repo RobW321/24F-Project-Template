24f84f68ba67ecb7aed709e0f3944f890734fe0a
MatthewFaust <faust.m@northeastern.edu>
2024-12-04 18:47:59 -0500

    abstracted josh and kyrie

--- app/src/pages/07_Josh_Home.py
+++ app/src/pages/07_Josh_Home.py
@@ -14,10 +14,15 @@ st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
-if st.button('View Applications', 
-             type='primary',
-             use_container_width=True):
-  st.switch_page('pages/14_Josh_Get_Apps.py')
+if st.button(
+    'View Applications',
+    type='primary',
+    use_container_width=True
+):
+    # Set the student_nuid value in session state
+    st.session_state.student_nuid = 1002
+    # Navigate to the page displaying applications
+    st.switch_page('pages/12_API_Test.py')
 
 if st.button('View World Map Demo', 
              type='primary',
