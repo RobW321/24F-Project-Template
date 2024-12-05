2766dcf84abf7dbc2e225c2efbba71e83abcd7ef
Robel Wondwosen <robelw135@gmail.com>
2024-12-05 15:14:06 -0500

    Adding filtering applications to Kyrie

--- app/src/pages/16_Kyrie_Job_Applications.py
+++ app/src/pages/16_Kyrie_Job_Applications.py
@@ -24,17 +24,32 @@ if st.button(
 if st.button('Add Applications', 
              type='primary',
              use_container_width=True):
+  st.session_state.student_nuid = 1004
   st.switch_page('pages/12_Add_Applications.py')
 
 if st.button('Edit Applications', 
              type='primary',
              use_container_width=True):
+  st.session_state.student_nuid = 1004
   st.switch_page('pages/12_Update_Application.py')
 
 if st.button('Delete Applications', 
              type='primary',
              use_container_width=True):
+  st.session_state.student_nuid = 1004
   st.switch_page('pages/12_Delete_Application.py')
+
+if st.button('Filter Applications by Priority',
+            type='primary',
+            use_container_width=True):
+    st.session_state.student_nuid = 1004
+    st.switch_page('pages/12_Filter_Application.py')
+    
+if st.button('Filter Applications by Status',
+            type='primary',
+            use_container_width=True):
+    st.session_state.student_nuid = 1004
+    st.switch_page('pages/12_Filter_By_Status_Application.py')
 
 if "student_nuid" not in st.session_state:
     st.session_state.student_nuid = None
