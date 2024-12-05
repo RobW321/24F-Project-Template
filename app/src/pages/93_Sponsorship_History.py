c50a619e22578ddc2ef4637f5f4fd7ce8951ef5d
christian-bacalhau <cbacalhau2005@gmail.com>
2024-12-05 17:12:03 -0500

    edited sponsorship history

--- app/src/pages/93_Sponsorship_History.py
+++ app/src/pages/93_Sponsorship_History.py
@@ -15,11 +15,11 @@ Viewing the Sponsorship History for All Companies with Applications
 data = {} 
 try:
   data = requests.get('http://api:4000/co/companies').json()
+
+  st.table(data)
 except:
   st.write("**Important**: Could not connect to sample api, so using dummy data.")
   data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
-
-st.dataframe(data)
 
 
 
